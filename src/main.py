00e083c15e16c0c87557f1cc8f4ff027cc68b8da
mdeboute <martin.deboute@gmail.com>
2022-11-25 15:32:37 +0100

    add a helper in the cli

--- src/main.py
+++ src/main.py
@@ -2,15 +2,21 @@ import sys
 from KEPData import KEPData
 
 
+def print_usage():
+    print("Usage: python3 main.py <instance_file>")
+    exit(0)
+
+
 def main():
     argv = sys.argv[1:]
 
     if len(argv) != 1:
-        print("Usage: python3 main.py <instance_file>")
-        exit(1)
+        print_usage()
+
+    if "-h" in argv or "--help" in argv:
+        print_usage()
 
     file_path = argv[0]
-
     instance = KEPData(file_path=file_path)
     print(instance)
 
